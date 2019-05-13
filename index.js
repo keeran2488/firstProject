0400882a759acb98f7718dc937700cef7737e10d
keeran2488 <chaudharyk456@gmail.com>
2019-05-13 21:11:43 +0545

    Added typewriter effect

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 var pos = 0;
-var turn = 0
-var text =['World!', 'Friends!', 'Family!'];
+var text =['Morning!','After Noon' ,'Evening!', 'Night!'];
+var today = new Date();
+var time = today.getHours();
+
+function turn(time){
+    if(time >= 4 && time <= 11 ){
+        return 0;
+    }
+    else if(time >= 12 && time <= 17){
+        return 1;
+    }
+    else if(time >= 18 && time <= 22){
+        return 2;
+    }
+    else{
+        return 3;
+    }
+}
+
+var toPrint = turn(time);
 
 function typing(){
-    if(pos<text[turn].length){
-        document.getElementById("greet").innerHTML += text[turn].charAt(pos);
+    if(pos<text[toPrint].length){
+        document.getElementById("greet").innerHTML += text[toPrint].charAt(pos);
         pos++;
         setTimeout(typing,120);
     }   
@@ -15,15 +33,14 @@ function typing(){
 
 function erase(){
     if(pos >=0){
-        var temp = text[turn].toString().substring(0, pos);
+        var temp = text[toPrint].toString().substring(0, pos);
         document.getElementById("greet").innerHTML = temp;
         pos--;
         setTimeout(erase, 200);
     }
     else{
-        turn++;
-        if(turn>=text.length){
-            turn = 0;
+        if(toPrint>=text.length){
+            toPrint = turn(time);
         }
         setTimeout(typing, 150);
     }
